3e5ea1da59dc53f060e42732c0512f3882bebd3c
Ghosuto <clash.raja10@gmail.com>
2026-08-01 14:41:33 +0000

    Restrict Dynamic Island setting to centered-cutout devices
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- src/com/crdroid/settings/fragments/StatusBar.java
+++ src/com/crdroid/settings/fragments/StatusBar.java
@@ -69,6 +69,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_CARRIER_KEY = "status_bar_carrier_key";
     private static final String CARRIER_NAME = "lockscreen_show_carrier";
     private static final String CUSTOM_CARRIER_LABEL = "lockscreen_show_custom_carrier_text";
+    private static final String DYNAMIC_ISLAND = "status_bar_dynamic_island";
 
     private static final int PULLDOWN_DIR_NONE = 0;
     private static final int PULLDOWN_DIR_RIGHT = 1;
@@ -79,6 +80,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private LineageSystemSettingListPreference mQuickPulldown;
 
     private Preference mCustomCarrierTextPref;
+    private Preference mDynamicIsland;
     private String mCustomCarrierText;
 
     @Override
@@ -107,6 +109,11 @@ public class StatusBar extends SettingsPreferenceFragment implements
         } else if (DeviceUtils.hasCenteredCutout(mContext)) {
             mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_notch);
             mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch);
+        }
+
+        mDynamicIsland = findPreference(DYNAMIC_ISLAND);
+        if (mDynamicIsland != null) {
+            mDynamicIsland.setVisible(DeviceUtils.hasCenteredCutout(mContext));
         }
 
         mQuickPulldown =
@@ -291,6 +298,10 @@ public class StatusBar extends SettingsPreferenceFragment implements
                     if (!TelephonyUtils.isVoiceCapable(context)) {
                         keys.add(CARRIER_NAME);
                         keys.add(CUSTOM_CARRIER_LABEL);
+                    }
+
+                    if (!DeviceUtils.hasCenteredCutout(context)) {
+                        keys.add(DYNAMIC_ISLAND);
                     }
                     return keys;
                 }
